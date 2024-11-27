020ce863f2502d4641393818e6d58d3035076a02
root <root@DESKTOP-UFHV2A9>
2024-11-27 16:45:15 +0100

    test

---
+++ cpp_linter_hooks/cppcheck.py
@@ -0,0 +1,39 @@
+import subprocess
+from argparse import ArgumentParser
+from typing import Tuple
+
+from .util import ensure_installed, DEFAULT_CLANG_VERSION
+
+
+parser = ArgumentParser()
+
+
+def run_cppcheck(args=None) -> Tuple[int, str]:
+    hook_args, other_args = parser.parse_known_args(args)
+    path = ensure_installed("cppcheck", "2.16")
+    command = [str(path)]
+    command.extend(other_args)
+
+    retval = 0
+    output = ""
+    try:
+        sp = subprocess.run(command, stdout=subprocess.PIPE, encoding='utf-8')
+        retval = sp.returncode
+        output = sp.stdout
+        if "warning:" in output or "error:" in output:
+            retval = 1
+        return retval, output
+    except FileNotFoundError as stderr:
+        retval = 1
+        return retval, str(stderr)
+
+
+def main() -> int:
+    retval, output = run_cppcheck()
+    if retval != 0:
+        print(output)
+    return retval
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
